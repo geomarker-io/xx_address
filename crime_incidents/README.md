21cff81f9b1dcf52f211209a9226e65a7cfdbdd1
erikarasnick <erika.rasnick@gmail.com>
2026-05-14 15:11:52 -0400

    updated street range matching

--- crime_incidents/README.md
+++ crime_incidents/README.md
@@ -7,12 +7,19 @@
 The `crime incidents` data package includes the date and street range locations of crime incidents from [PDI (Police Data Initiative) Crime Incidents](https://data.cincinnati-oh.gov/safety/PDI-Police-Data-Initiative-Crime-Incidents/k59e-2pvf) reported between January 1, 2011 and August 3, 2025. Incidents are the records, of reported crimes, collated by an agency for management. Incidents are typically housed in a Records Management System (RMS) that stores agency-wide data about law enforcement operations. This does not include police calls for service, arrest information, final case determination, or any other incident outcome data. 
 
 Crime incident information includes: 
+* `incident_no`: incident identifier (one incident can have multiple rows; Each id represents a victim of a crime.)
 * `date_time`: the estimated date/time of the start of the crime, filtered to included crime incidents that occurred on or after January 1, 2011
+* `hate_bias`: If it is deemed a hate crime, this field will indicate what group the hate crime was aimed at.
 * `address_x`: the administratively masked location of the incident
 * `lon_jittered` and `lat_jittered`: randomly skewed coordinates (represent the same block area, but not the exact location, of the incident)
 * `category`: based on the reported offense; one of `property` (e.g., burglary, larceny, motor vehicle theft, arson), `violent` (e.g., homicide, assault, rape, robbery, domestic violence), or `other` (e.g., missing/unknown, fraud, identify theft, consensual crime)
 * `xx_address`: `address_x` with `XX` replaced with `50` to estimate the middle of the street block
-* `geometry`: the sfc geometry for the geocoded TIGER street range
-* `from` and `to`: the minimum and maximum building numbers for the geocoded street range
+* `row_id`: unique row identifier
+* `addr_string`: the name of the matched TIGER street
+* `ZIP`: zipcode that contains the matched TIGER street
+* `addr_street`: addr_street components (see geomarker.io/addr)
+* `FROMHN` and `TOHN`: the minimum and maximum building numbers for the geocoded street range
+* `s2_geography`: the sfc geometry for the geocoded TIGER street range
+
 
 
--- crime_incidents/make_crime_incidents.R
+++ crime_incidents/make_crime_incidents.R
@@ -9,6 +9,7 @@ library(addr)
 raw_data <- read_csv(
   "https://data.cincinnati-oh.gov/api/views/k59e-2pvf/rows.csv?accessType=DOWNLOAD",
   col_types = cols_only(
+    INCIDENT_NO = col_character(),
     DATE_FROM = col_datetime(format = "%m/%d/%Y %I:%M:%S %p"),
     OFFENSE = col_character(),
     ADDRESS_X = col_character(),
@@ -18,6 +19,7 @@ raw_data <- read_csv(
   )
 ) |>
   rename(
+    incident_no = INCIDENT_NO,
     date_time = DATE_FROM,
     offense = OFFENSE,
     address_x = ADDRESS_X,
@@ -25,6 +27,12 @@ raw_data <- read_csv(
     lon_jittered = LONGITUDE_X,
     hate_bias = HATE_BIAS
   )
+# same incident, different jittered coords -> combine
+# group_by(incident_no, date_time, offense, address_x, hate_bias) |>
+# summarize(across(c(lat_jittered, lon_jittered), \(x) {
+#   mean(x, na.rm = TRUE)
+# })) |>
+# ungroup()
 
 min(raw_data$date_time, na.rm = TRUE)
 max(raw_data$date_time, na.rm = TRUE)
@@ -61,43 +69,285 @@ d <-
   ) |>
   select(-address_x_spl, -address_x_num, -address_x_street)
 
-tiger_street_ranges <- tiger_addr_feat("39061", year = "2025") |>
-  st_as_sf()
+d$row_id <- 1:nrow(d)
 
-tiger_street_names <- tiger_feat_names("39061", "2025") |>
-  mutate(addr_street_string = as.character(addr_street))
+# bring in street range geometries and summarize/unionize overlapping ranges
+addr::taf_install("39061", "2025")
 
-d_lineid <-
-  d |>
-  mutate(
-    addr_street_string = as.character(match_addr_street(
-      addr@street,
-      tiger_street_names$addr_street
-    ))
+tiger_streets <-
+  addr::taf_zip(c(
+    cincy::zcta_tigris_2010$zcta_2010,
+    cincy::zcta_tigris_2020$zcta_2020
+  )) |>
+  st_as_sf() |>
+  arrange(LINEARID, FROMHN) |>
+  group_by(
+    LINEARID,
+    FULLNAME,
+    ZIP,
+    OFFSET,
+    county_fips,
+    street_tag_parsed,
+    addr_street
   ) |>
-  left_join(tiger_street_names, by = "addr_street_string")
+  mutate(
+    prev_to = lag(cummax(as.numeric(TOHN))),
+    new_group = FROMHN > prev_to,
+    new_group = replace_na(new_group, FALSE),
+    range_id = cumsum(new_group) + 1
+  ) |>
+  ungroup() |>
+  group_by(
+    LINEARID,
+    FULLNAME,
+    ZIP,
+    OFFSET,
+    county_fips,
+    street_tag_parsed,
+    addr_street,
+    range_id
+  ) |>
+  summarize(
+    FROMHN = min(FROMHN),
+    TOHN = max(TOHN),
+    s2_geography = st_union(s2_geography)
+  ) |>
+  select(-range_id) |>
+  ungroup() |>
+  mutate(addr_string = as.character(addr_street))
+
+# match addr_street from crimes to addr_street in tiger_streets
+unique_xx_adds <-
+  d |>
+  select(xx_address, addr) |>
+  group_by(xx_address) |>
+  slice(1) |>
+  ungroup()
 
 d_matched_range <-
-  d_lineid |>
-  left_join(tiger_street_ranges, by = "LINEARID") |>
-  filter(FROMHN < addr@number@digits, TOHN > addr@number@digits) |>
-  group_by(date_time, address_x) |>
-  summarize(
-    from = min(FROMHN),
-    to = max(TOHN),
-    s2_geography = sf::st_union(s2_geography)
+  unique_xx_adds |>
+  mutate(
+    addr_string = as.character(match_addr_street(
+      addr@street,
+      tiger_streets$addr_street
+    ))
+  ) |>
+  left_join(tiger_streets, by = "addr_string") |>
+  filter(
+    FROMHN <= as.numeric(addr@number@digits) &
+      TOHN >= as.numeric(addr@number@digits)
   )
 
-d <- left_join(d, d_matched_range, by = c("date_time", "address_x"))
+unique_xx_adds <-
+  left_join(unique_xx_adds, d_matched_range |> select(-addr), by = "xx_address")
+
+d_matched <- left_join(d, unique_xx_adds |> select(-addr), by = "xx_address")
+
+# handle multimatches
+# if in same ZIP, union
+# if in different ZIP, take closest to jittered coords?
+# if in different ZIP but same line, union
+d_matched <-
+  d_matched |>
+  group_by(
+    row_id,
+    incident_no,
+    date_time,
+    address_x,
+    hate_bias,
+    lat_jittered,
+    lon_jittered,
+    category,
+    xx_address,
+    addr
+  ) |>
+  mutate(
+    n_matches = ifelse(!is.na(addr_string), n(), 0),
+    n_zip = n_distinct(ZIP),
+    n_linearid = n_distinct(LINEARID),
+    match_type = case_when(
+      n_matches == 0 ~ "no_match",
+      n_matches == 1 ~ "single_match",
+      n_matches > 1 & n_zip == 1 ~ "same_zip",
+      n_matches > 1 & n_linearid == 1 & n_zip > 1 ~ "different_zip_same_line",
+      n_matches > 1 & n_linearid > 1 & n_zip > 1 ~ "different_zip"
+    )
+  ) |>
+  ungroup()
+
+d_matched |>
+  group_by(match_type) |>
+  tally() |>
+  mutate(pct = n / sum(n) * 100)
+
+d_matched <- split(d_matched, f = d_matched$match_type)
+
+d_matched$no_match <-
+  d_matched$no_match |>
+  select(-addr) |>
+  mutate(addr_street = as.data.frame(addr_street))
+
+d_matched$single_match <-
+  d_matched$single_match |>
+  select(-addr) |>
+  mutate(addr_street = as.data.frame(addr_street))
+
+d_matched$same_zip <-
+  d_matched$same_zip |>
+  group_by(
+    row_id,
+    incident_no,
+    date_time,
+    address_x,
+    hate_bias,
+    lat_jittered,
+    lon_jittered,
+    category,
+    xx_address,
+    addr,
+    FULLNAME,
+    ZIP,
+    county_fips,
+    addr_street,
+    n_matches,
+    n_zip,
+    match_type
+  ) |>
+  summarize(
+    FROMHN = min(FROMHN),
+    TOHN = max(TOHN),
+    s2_geography = st_union(s2_geography)
+  ) |>
+  ungroup() |>
+  select(-addr) |>
+  mutate(addr_street = as.data.frame(addr_street))
+
+d_matched$different_zip_same_line <-
+  d_matched$different_zip_same_line |>
+  group_by(
+    row_id,
+    incident_no,
+    date_time,
+    address_x,
+    hate_bias,
+    lat_jittered,
+    lon_jittered,
+    category,
+    xx_address,
+    addr,
+    FULLNAME,
+    county_fips,
+    addr_street,
+    n_matches,
+    n_zip,
+    match_type
+  ) |>
+  summarize(
+    FROMHN = min(FROMHN),
+    TOHN = max(TOHN),
+    s2_geography = st_union(s2_geography)
+  ) |>
+  ungroup() |>
+  select(-addr) |>
+  mutate(addr_street = as.data.frame(addr_street))
+
+# if missing jittered coords, take first
+d_matched_diff_zip_missing_coords <-
+  d_matched$different_zip |>
+  filter(is.na(lat_jittered)) |>
+  group_by(
+    row_id,
+    incident_no,
+    date_time,
+    address_x,
+    hate_bias,
+    lat_jittered,
+    lon_jittered,
+    category,
+    xx_address,
+    addr,
+    FULLNAME,
+    county_fips,
+    addr_street,
+    n_matches,
+    n_zip,
+    match_type
+  ) |>
+  # if min distance is tied, take the first one
+  slice(1) |>
+  ungroup() |>
+  select(-addr) |>
+  mutate(addr_street = as.data.frame(addr_street))
+
+d_matched_diff_zip <-
+  d_matched$different_zip |>
+  filter(!is.na(lat_jittered), !is.na(lon_jittered)) |>
+  mutate(lat = lat_jittered, lon = lon_jittered) |>
+  st_as_sf(coords = c("lon", "lat"), crs = 4326) |>
+  rename(point_geom = geometry) |>
+  mutate(
+    distance_m = as.numeric(st_distance(
+      point_geom,
+      s2_geography,
+      by_element = TRUE
+    ))
+  ) |>
+  st_drop_geometry() |>
+  group_by(
+    row_id,
+    incident_no,
+    date_time,
+    address_x,
+    hate_bias,
+    lat_jittered,
+    lon_jittered,
+    category,
+    xx_address,
+    addr,
+    FULLNAME,
+    county_fips,
+    addr_street,
+    n_matches,
+    n_zip,
+    match_type
+  ) |>
+  filter(distance_m == min(distance_m)) |>
+  # if min distance is tied, take the first one
+  slice(1) |>
+  ungroup() |>
+  select(-addr) |>
+  mutate(addr_street = as.data.frame(addr_street))
+
+d_matched$different_zip <- bind_rows(
+  d_matched_diff_zip_missing_coords,
+  d_matched_diff_zip
+)
+
+d_matched <-
+  bind_rows(d_matched) |>
+  arrange(incident_no, date_time) |>
+  select(
+    -FULLNAME,
+    -LINEARID,
+    -OFFSET,
+    -county_fips,
+    -street_tag_parsed,
+    -n_matches,
+    -n_zip,
+    -match_type,
+    -distance_m,
+    -n_linearid
+  )
+
+glimpse(d_matched)
 
 d_dpkg <-
-  d |>
+  d_matched |>
   rename(geometry = s2_geography) |>
-  select(-addr) |>
   dpkg::as_dpkg(
     name = "crime_incidents",
     title = "Crime Incidents",
-    version = "0.2.0",
+    version = "1.0.0",
     homepage = "https://github.com/geomarker-io/xx_address",
     description = paste(
       readLines(fs::path("crime_incidents", "README", ext = "md")),
